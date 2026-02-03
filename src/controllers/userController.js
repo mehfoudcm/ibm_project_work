996f84f209a01839ef65cd9b4c2b1da17b3b49b4
mehfoudcm <mehfoudcm@vcu.edu>
2026-02-02 21:12:33 -0500

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,3 +2,16 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
+// Register a new user
+exports.registerUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({ message: 'User registered successfully.' });
+    } catch (error) {
+        res.status(500).json({ error: 'Registration failed.' });
+    }
+};
